fb30e3ba88b9913c59a1ed305d0bfc34e69b3711
Nishatreza22 <110771409+Nishatreza22@users.noreply.github.com>
2022-08-24 11:33:00 -0400

    Update ArrayDemo.java

--- src/com/javabasic/ArrayDemo.java
+++ src/com/javabasic/ArrayDemo.java
@@ -1,4 +1,23 @@
 package com.javabasic;
 
 public class ArrayDemo {
+  
+        int[] mn = new int[5];
+        mn[0] = 100;
+        mn[1] = 125;
+        mn[2] = 135;
+        mn[3] = 145;
+        mn[4] = 155;
+
+        String[] name = new String[6];
+        name[0] = "Nishat";
+        name[1] = "Shafi";
+        name[2] = "Sadman";
+        name[3] = "Kanon";
+        name[4] = "Kanok";
+        name[5] = "Nihal";
+
+
+        System.out.println(name[4]);
+    
 }
